b2d35e5783039be34a9a783b2e86bae3c5d9b8b0
cans98 <53710530+cans98@users.noreply.github.com>
2021-01-10 22:10:11 +0300

    Add files via upload

---
+++ index_bul/index_bul/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace index_bul
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] sehirler = { "eskisehir", "ankara", "istanbul", "izmir", "sinop", "diyarbakir", "zonguldak", "tekirdag", "kocaeli" };
+            Console.WriteLine("şehir gir");
+            string girilensehir = Console.ReadLine();
+            bool varMi = false;
+            int n = 0;
+            foreach (string sehir in sehirler)
+            {
+
+
+                n = n + 1; // index için tanımlanan counter
+                if (sehir == girilensehir)
+                {
+                    varMi = true;
+                    Console.WriteLine($"{girilensehir} arrayda var  ");
+                    Console.WriteLine($"index numarası {n}");
+                    break; // bi kere bulunca çık
+
+                }
+               
+            }
+            if (varMi == false) //surekli arrayda yok yazdırmaması için true false kontrolunu en son yaptım
+            {
+                Console.WriteLine($"{girilensehir} arrayda yok  "); 
+            }
+
+         
+            Console.ReadKey();
+        }
+    }
+}
